09d292dfc5c37f63ed2e19ef0a5fbb0d1ea2bd70
yiweihuang <n1993212@gmail.com>
2017-01-02 22:40:03 +0800

    heroku error

--- config/environments.rb
+++ config/environments.rb
@@ -4,3 +4,8 @@ configure :development, :test do
   require 'config_env'
   ConfigEnv.path_to_config("#{__dir__}/config_env.rb")
 end
+
+configure :production do
+  # ENV['DATABASE_URL'] should be set by Heroku
+  # Configuration settings should be directly set on Heroku
+end
